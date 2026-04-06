48a1c9059b7e0e0eba04adb0294d25451104e20f
rafaelcgs10 <rafaelcgs10@gmail.com>
2026-04-06 11:07:43 +0200

    Spektrafilm building

--- default.nix
+++ default.nix
@@ -9,6 +9,26 @@
 
 { pkgs ? import <nixpkgs> { } }:
 
+let
+  spektrafilm-python = import ./pkgs/spektrafilm/python-runtime.nix;
+  spektrafilm-pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/25.05.tar.gz";
+  }) {
+    config.allowBroken = true;
+    overlays = [
+      (final: prev: {
+        pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+          (python-final: python-prev: {
+            colour-science = import ./pkgs/spektrafilm/colour-science.nix { pkgs = final; };
+            pyfftw = import ./pkgs/spektrafilm/pyfftw.nix { pkgs = final; };
+            openimageio = import ./pkgs/spektrafilm/openimageio.nix { pkgs = final; };
+            spektrafilm = import ./pkgs/spektrafilm/spektrafilm.nix { pkgs = final; };
+          })
+        ];
+      })
+    ];
+  };
+in
 {
   # The `lib`, `overlays`, `nixosModules`, `homeModules`,
   # `darwinModules` and `flakeModules` names are special
@@ -19,7 +39,8 @@
   # flakeModules = { }; # flake-parts modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  spektrafilm = spektrafilm-pkgs.python3Packages.spektrafilm;
+  # example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
